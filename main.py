6fcbf19625386a4f6b800ceced3318a923a7458b
thomas <vigroux.thomas82@gmail.com>
2021-11-11 17:28:55 +0100

    Fix sort methods

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ test_list = [2, 4, 19, 49, 23, 12, 7, 79, 102, 32, 65, 69, 15, 51, 6, 129, -3, 5
 print("base {}".format(test_list))
 
 QS = quicksort.QuickSort(test_list)
-print(QS.quicksort(test_list, 0, len(test_list)-1))
+print(QS.quicksort(0, len(test_list)-1))
 
 print("base {}".format(test_list))
 
--- sort/quicksort.py
+++ sort/quicksort.py
@@ -1,24 +1,24 @@
 class QuickSort:
-    def __init__(self, list):
-        self.list_result = [x for x in list]
+    def __init__(self, array):
+        self.list_result = [x for x in array]
 
-    def partition(self, list, start, end):
+    def partition(self, array, start, end):
         i = (start - 1)
-        pivot = list[end]
+        pivot = array[end]
         for j in range (start, end):
-            if list[j] <= pivot:
+            if array[j] <= pivot:
                 i = i + 1
-                list[i], list[j] = list[j], list[i]
-        list[i+1], list[end] = list[end], list[i+1]
+                array[i], array[j] = array[j], array[i]
+        array[i+1], array[end] = array[end], array[i+1]
         return i+1
 
-    def quicksort(self, list, start, end):
+    def quicksort(self, start, end):
         if len(self.list_result) == 1:
             return self.list_result
         if start < end:
             pi = self.partition(self.list_result, start, end)
-            self.quicksort(self.list_result, start, pi-1)
-            self.quicksort(self.list_result, pi+1, end)
+            self.quicksort(start, pi-1)
+            self.quicksort(pi+1, end)
         return self.list_result
 
 
